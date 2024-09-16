c5361c8f2d6bd7c349e2fce5f0087bbcdd8b92ac
saim hashmi <hashmisaim037@gmail.com>
2024-09-16 15:12:41 -0400

    Connecting to Mongodb

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const mongoURI = "mongodb://localhost:27017/"
 
 async function connectToMongo() {
-    await mongoose.connect(mongoURI).then(()=> console.log("Connected to Mongo Successfully")).catch(err => console.log(err));
-  }
+  await mongoose.connect(mongoURI).then(()=> console.log("Connected to Mongo Successfully")).catch(err => console.log(err));
+}
 
 module.exports = connectToMongo
